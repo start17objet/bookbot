e3b51b16beb06d103abaea6743a33f52613fbb0a
start17objet <start17objet@gmail.com>
2025-03-17 21:51:57 +0200

    add func to calc num of words in string

--- main.py
+++ main.py
@@ -2,17 +2,20 @@
 
 
 def get_book_contents(file_path):
-    # func gets string from file.txt
     with open(file_path, "rt") as file:
         contents = file.read()
     return contents
 
+def get_word_amount(str):
+    words_arr = str.split()
+    return len(words_arr)
 
 def main():
     
     file_path_relative = "./books/frankenstein.txt"
     book_contents = get_book_contents(file_path_relative)
-    print(book_contents)
+    word_amount = get_word_amount(book_contents)
+    print(f"{word_amount} words found in the document")
 
 
 main()
