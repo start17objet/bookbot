538f3f1e2c9f873373b576dabc8bd9e23ad4ea02
start17objet <start17objet@gmail.com>
2025-03-19 18:57:44 +0200

    add arguments to main, finished

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from stats import get_book_contents, get_word_amount, get_book_stats, sort_stats
-
+import sys
 
 def main():
     
-    file_path_relative = "./books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    file_path_relative = sys.argv[1]
     book_contents = get_book_contents(file_path_relative)
     word_amount = get_word_amount(book_contents)
     book_stats = sort_stats(get_book_stats(book_contents))
